77b03a4e994bd4296f4ddbc5a58d61af46620ad5
Humberto L. Varona <humberto.varona@yandex.com>
2023-05-30 21:05:51 -0300

    Create convert_distance.c

---
+++ convert_distance.c
@@ -0,0 +1,12 @@
+double convert_distance(double distance, char* unit) {
+    if (strcmp(unit, "km") == 0) {
+        return distance * 111.12;
+    } else if (strcmp(unit, "miles") == 0) {
+        return distance * 69.046766881413;
+    } else if (strcmp(unit, "nautical miles") == 0) {
+        return distance * 60;
+    } else {
+        printf("Invalid unit. Supported units are 'km', 'miles', and 'nm'.\n");
+        exit(1);
+    }
+}
